ee0413fd20a1499990bf6192b89d71139b7c4a30
Pierre Schmitz <pierre@archlinux.de>
2018-08-11 22:31:08 +0200

    Add more tests

--- tests/Controller/PostPackageListControllerTest.php
+++ tests/Controller/PostPackageListControllerTest.php
@@ -9,17 +9,105 @@ use App\Tests\Util\DatabaseTestCase;
  */
 class PostPackageListControllerTest extends DatabaseTestCase
 {
-    public function testPostPackageListIsSuccessful()
+    /**
+     * @param string $version
+     * @dataProvider provideSupportedVersions
+     */
+    public function testPostPackageListIsSuccessful(string $version)
     {
         $client = $this->getClient();
 
         $client->request(
             'POST',
             '/post',
-            ['pkgstatsver' => '2.3', 'arch' => 'x86_64', 'packages' => 'pkgstats', 'modules' => 'snd']
+            ['pkgstatsver' => $version, 'arch' => 'x86_64', 'packages' => 'pkgstats', 'modules' => 'snd']
         );
 
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertContains('Thanks for your submission. :-)', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @return array
+     */
+    public function provideSupportedVersions(): array
+    {
+        return [
+            ['1.0'],
+            ['2.0'],
+            ['2.1'],
+            ['2.2'],
+            ['2.3']
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function provideUnsupportedVersions(): array
+    {
+        return [
+            ['0.1'],
+            [''],
+            ['a'],
+            ['1.0alpha'],
+            ['42']
+        ];
+    }
+
+    /**
+     * @param string $version
+     * @dataProvider provideUnsupportedVersions
+     */
+    public function testUnsupportedVersionFails(string $version)
+    {
+        $client = $this->getClient();
+
+        $client->request(
+            'POST',
+            '/post',
+            ['pkgstatsver' => $version, 'arch' => 'x86_64', 'packages' => 'pkgstats', 'modules' => 'snd']
+        );
+
+        $this->assertTrue($client->getResponse()->isClientError());
+    }
+
+    /**
+     * @param string $architecture
+     * @dataProvider provideSupportedArchitectures
+     */
+    public function testPostPackageListWithArchitectureIsSuccessful(string $architecture)
+    {
+        $client = $this->getClient();
+
+        $client->request(
+            'POST',
+            '/post',
+            ['pkgstatsver' => '2.3', 'arch' => $architecture, 'packages' => 'pkgstats', 'modules' => 'snd']
+        );
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * @return array
+     */
+    public function provideSupportedArchitectures(): array
+    {
+        return [
+            ['i686'],
+            ['x86_64']
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function provideUnsupportedArchitectures(): array
+    {
+        return [
+            [''],
+            ['arm']
+        ];
     }
 }
