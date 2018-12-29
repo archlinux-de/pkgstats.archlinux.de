c0a840e709d56ab5efa057166742935c6d47a83d
Pierre Schmitz <pierre@archlinux.de>
2018-12-29 06:46:51 +0100

    Add basic bundle tests

---
+++ lib/DatatablesApiBundle/Tests/Entity/TestEntity.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace DatatablesApiBundle\Tests\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class TestEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $value;
+
+    /**
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
