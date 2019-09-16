2a512a558720c426d99ccbb5a216407c09b8a124
Pierre Schmitz <pierre@archlinux.de>
2019-09-16 20:53:52 +0200

    Use Shadow DOM for Swagger UI if available

--- assets/js/swagger_ui.js
+++ assets/js/swagger_ui.js
@@ -1,11 +1,20 @@
+/* eslint-env browser */
 import SwaggerUI from 'swagger-ui'
+import Styles from '!css-loader!postcss-loader!sass-loader!../css/swagger_ui.scss' // eslint-disable-line
 
-window.onload = () => {
-  const swaggerUi = document.getElementById('swagger-ui')
-  const swaggerUrl = swaggerUi.dataset.swaggerUrl
-  SwaggerUI({
-    domNode: swaggerUi,
-    url: swaggerUrl,
-    defaultModelsExpandDepth: 0
-  })
+let domNode = document.getElementById('swagger-ui')
+const url = domNode.dataset.url
+
+if (HTMLElement.prototype.attachShadow) {
+  domNode = domNode.attachShadow({ mode: 'open' })
 }
+
+SwaggerUI({
+  domNode: domNode,
+  url: url,
+  defaultModelsExpandDepth: 0
+})
+
+const style = document.createElement('style')
+style.innerHTML = Styles
+domNode.appendChild(style)
