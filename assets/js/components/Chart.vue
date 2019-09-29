24630a22002445aabbfc97587282ce377669b15e
Pierre Schmitz <pierre@archlinux.de>
2019-09-29 09:13:05 +0200

    Fix code style

--- assets/js/components/Chart.vue
+++ assets/js/components/Chart.vue
@@ -5,92 +5,92 @@
 </template>
 
 <script>
-    import Chartist from 'chartist'
-    import 'chartist-plugin-legend'
-    // support IE 11
-    import 'whatwg-fetch'
-    import {convertToDataSeries} from "../services/DataSeriesConverter";
+  import Chartist from 'chartist'
+  import 'chartist-plugin-legend'
+  // support IE 11
+  import 'whatwg-fetch'
+  import { convertToDataSeries } from '../services/DataSeriesConverter'
 
-    export default {
-        name: 'Chart',
-        data() {
-            return {
-                loading: true,
-                data: [],
-                urls: this.$parent.$data.urls
-            }
-        },
-        watch: {
-            urls: function () {
-                this.fetchData()
+  export default {
+    name: 'Chart',
+    data () {
+      return {
+        loading: true,
+        data: [],
+        urls: this.$parent.$data.urls
+      }
+    },
+    watch: {
+      urls: function () {
+        this.fetchData()
+      },
+      data: function () {
+        this.drawChart()
+      }
+    },
+    methods: {
+      fetchData: function () {
+        this.loading = true
+        Promise.all(this.urls.map(url => {
+          return fetch(url, {
+            credentials: 'omit',
+            headers: new Headers({ Accept: 'application/json' })
+          }).then(response => response.json())
+        }))
+          .then(dataArray => {
+            dataArray.forEach(data => {
+              if (!data.count) {
+                throw new Error('No package data found')
+              }
+            })
+            this.data = convertToDataSeries(dataArray)
+            this.loading = false
+          })
+          .catch(e => {
+            this.loading = false
+            const error = document.createElement('div')
+            error.classList.add('alert')
+            error.classList.add('alert-danger')
+            error.setAttribute('role', 'alert')
+            error.innerText = e.toString()
+            this.$el.appendChild(error)
+          })
+      },
+      drawChart: function () {
+        Chartist.Line(this.$el, this.data, {
+          showPoint: false,
+          showArea: this.data.series.length < 4,
+          chartPadding: {
+            top: 24
+          },
+          axisX: {
+            showGrid: false,
+            labelInterpolationFnc: value => value.toString().endsWith('01') && value.toString().slice(0, -2) % 2 === 0 ? value.toString().slice(0, -2) : null
+          },
+          plugins: this.data.series.length > 1 ? [
+            Chartist.plugins.legend({
+              clickable: false
+            })
+          ] : []
+        }, [
+          ['screen and (min-width: 576px)', {
+            chartPadding: {
+              top: 36
             },
-            data: function () {
-                this.drawChart()
+            axisX: {
+              labelInterpolationFnc: value => value.toString().endsWith('01') ? value.toString().slice(0, -2) : null
             }
-        },
-        methods: {
-            fetchData: function () {
-                this.loading = true
-                Promise.all(this.urls.map(url => {
-                    return fetch(url, {
-                        credentials: 'omit',
-                        headers: new Headers({Accept: 'application/json'})
-                    }).then(response => response.json())
-                }))
-                    .then(dataArray => {
-                        dataArray.forEach(data => {
-                            if (!data.count) {
-                                throw new Error('No package data found')
-                            }
-                        })
-                        this.data = convertToDataSeries(dataArray)
-                        this.loading = false
-                    })
-                    .catch(e => {
-                        this.loading = false
-                        const error = document.createElement('div')
-                        error.classList.add('alert')
-                        error.classList.add('alert-danger')
-                        error.setAttribute('role', 'alert')
-                        error.innerText = e.toString()
-                        this.$el.appendChild(error)
-                    })
-            },
-            drawChart: function () {
-                Chartist.Line(this.$el, this.data, {
-                    showPoint: false,
-                    showArea: this.data.series.length < 4,
-                    chartPadding: {
-                        top: 24
-                    },
-                    axisX: {
-                        showGrid: false,
-                        labelInterpolationFnc: value => value.toString().endsWith('01') && value.toString().slice(0, -2) % 2 === 0 ? value.toString().slice(0, -2) : null
-                    },
-                    plugins: this.data.series.length > 1 ? [
-                        Chartist.plugins.legend({
-                            clickable: false
-                        })
-                    ] : []
-                }, [
-                    ['screen and (min-width: 576px)', {
-                        chartPadding: {
-                            top: 36
-                        },
-                        axisX: {
-                            labelInterpolationFnc: value => value.toString().endsWith('01') ? value.toString().slice(0, -2) : null
-                        }
-                    }],
-                    ['screen and (min-width: 768px)', {
-                        chartPadding: {
-                            top: 48
-                        }
-                    }]
-                ])
+          }],
+          ['screen and (min-width: 768px)', {
+            chartPadding: {
+              top: 48
             }
-        },
-        mounted() {
-            this.fetchData()
-        }
+          }]
+        ])
+      }
+    },
+    mounted () {
+      this.fetchData()
     }
+  }
 </script>
