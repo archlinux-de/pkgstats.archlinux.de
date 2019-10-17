f60cba44dcf6cd600967abb0e92e44fbbe69b3e1
Pierre Schmitz <pierre@archlinux.de>
2019-10-17 22:47:03 +0200

    Do not initially load the spinner

--- assets/js/components/PackagesBarChart.vue
+++ assets/js/components/PackagesBarChart.vue
@@ -40,7 +40,7 @@ export default {
   },
   data () {
     return {
-      loading: true,
+      loading: false,
       packagePopularities: this.packages.map(pkg => ({ name: pkg, popularity: 0 })),
       error: ''
     }
@@ -58,6 +58,7 @@ export default {
       return pkgs.sort((a, b) => Math.sign(b.popularity - a.popularity))
     },
     fetchData () {
+      this.loading = true
       Promise.all(this.packages.map(pkg => this.apiPackagesService.fetchPackagePopularity(pkg)))
         .then(dataArray => { this.packagePopularities = this.sortPackagesByPopularity(dataArray) })
         .catch(error => { this.error = error })
