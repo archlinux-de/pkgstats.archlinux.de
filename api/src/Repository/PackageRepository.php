8be6095b9d3c8179e0b1e05ea70e3f3d6808d75b
Pierre Schmitz <pierre@archlinux.de>
2020-09-05 07:53:26 +0200

    Update dependencies

--- api/src/Repository/PackageRepository.php
+++ api/src/Repository/PackageRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Package;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
