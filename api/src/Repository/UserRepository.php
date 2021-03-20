3a80e28b99959955255129a08b04ef6fca21d36b
Pierre Schmitz <pierre@archlinux.de>
2021-03-20 08:07:02 +0100

    Use Symfony rate limiter instead of our own implementation

--- api/src/Repository/UserRepository.php
+++ api/src/Repository/UserRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\ORM\NoResultException;
 
 class UserRepository extends ServiceEntityRepository
 {
@@ -15,27 +14,5 @@ class UserRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
-    }
-
-    /**
-     * @param string $ip
-     * @param int $startTime
-     * @return int
-     */
-    public function getSubmissionCountSince(string $ip, int $startTime): int
-    {
-        try {
-            return $this->createQueryBuilder('user')
-                ->select('COUNT(user)')
-                ->where('user.time >= :startTime')
-                ->andWhere('user.ip = :ip')
-                ->groupBy('user.ip')
-                ->setParameter('startTime', $startTime)
-                ->setParameter('ip', $ip)
-                ->getQuery()
-                ->getSingleScalarResult();
-        } catch (NoResultException $e) {
-            return 0;
-        }
     }
 }
---
+++ api/tests/EventSubscriber/PkgstatsRequestRateLimitSubscriberTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Tests\EventSubscriber;
+
+use App\EventSubscriber\PkgstatsRequestRateLimitSubscriber;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\RateLimiter\Storage\InMemoryStorage;
+
+class PkgstatsRequestRateLimitSubscriberTest extends TestCase
+{
+    /** @var PkgstatsRequestRateLimitSubscriber */
+    private $rateLimitSubscriber;
+
+    public function setUp(): void
+    {
+        $pkgstatsRequestLimiter = new RateLimiterFactory(
+            ['id' => 'pkgstats_request', 'policy' => 'fixed_window', 'limit' => 1, 'interval' => '1 day'],
+            new InMemoryStorage()
+        );
+
+        $this->rateLimitSubscriber = new PkgstatsRequestRateLimitSubscriber($pkgstatsRequestLimiter);
+    }
+
+    public function testGetSubscribedEvents(): void
+    {
+        $events = $this->rateLimitSubscriber->getSubscribedEvents();
+
+        $this->assertArrayHasKey(KernelEvents::REQUEST, $events);
+    }
+
+    /**
+     * @param string $route
+     * @return RequestEvent
+     */
+    private function createEvent(string $route): RequestEvent
+    {
+        /** @var KernelInterface|MockObject $kernel */
+        $kernel = $this->createMock(KernelInterface::class);
+
+        /** @var Request|MockObject $request */
+        $request = $this->createMock(Request::class);
+        $request
+            ->expects($this->any())
+            ->method('getClientIp')
+            ->willReturn('127.0.0.1');
+        $request->attributes = new ParameterBag(['_route' => $route]);
+
+        return new RequestEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST);
+    }
+
+    /**
+     * @param string $route
+     * @dataProvider provideRateLimitedRoutes
+     */
+    public function testRateLimit(string $route): void
+    {
+        $event = $this->createEvent($route);
+        $this->rateLimitSubscriber->onKernelRequest($event);
+        $this->expectException(TooManyRequestsHttpException::class);
+        $this->rateLimitSubscriber->onKernelRequest($event);
+    }
+
+    public function testRateLimitDoesNotApplyToOtherRoutes(): void
+    {
+        $event = $this->createEvent('foo');
+        $this->rateLimitSubscriber->onKernelRequest($event);
+        $this->rateLimitSubscriber->onKernelRequest($event);
+        // Asserting that not exception was thrown
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function provideRateLimitedRoutes(): array
+    {
+        return [
+            ['app_api_submit'],
+            ['app_pkgstats_post']
+        ];
+    }
+}
