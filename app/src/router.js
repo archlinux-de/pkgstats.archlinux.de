13ead21a30e0612f49cf1529d0b17bc5c8561a4b
Pierre Schmitz <pierre@archlinux.de>
2021-09-10 08:39:32 +0200

    Move package chart into its own chunk

--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Compare from './views/Compare'
 import Fun from './views/Fun'
 import Impressum from './views/Impressum'
-import Package from './views/Package'
 import Packages from './views/Packages'
 import PrivacyPolicy from './views/PrivacyPolicy'
 import Start from './views/Start'
@@ -16,10 +14,10 @@ export default new Router({
   mode: 'history',
   linkActiveClass: 'active',
   routes: [
-    { path: '/compare/packages', name: 'compare', component: Compare },
+    { path: '/compare/packages', name: 'compare', component: () => import(/* webpackChunkName: "package-chart" */ './views/Compare') },
     { path: '/fun', name: 'fun', component: Fun },
     { path: '/impressum', name: 'impressum', component: Impressum },
-    { path: '/packages/:package', name: 'package', component: Package },
+    { path: '/packages/:package', name: 'package', component: () => import(/* webpackChunkName: "package-chart" */ './views/Package') },
     { path: '/packages', name: 'packages', component: Packages },
     { path: '/privacy-policy', name: 'privacy-policy', component: PrivacyPolicy },
     { path: '/', name: 'start', component: Start },
