46ecd839d0eee3632385fcf21e82fdd2174a9a7f
Pierre Schmitz <pierre@archlinux.de>
2021-09-10 09:22:18 +0200

    Stop transpiling node_modules as it breaks swagger-ui

--- app/webpack.prod.js
+++ app/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
 
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.s?css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'] }
     ]
   },
