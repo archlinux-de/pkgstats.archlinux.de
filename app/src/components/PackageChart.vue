c025bd283176c172d689ad2942b7bca870410c28
Pierre Schmitz <pierre@archlinux.de>
2021-09-13 08:43:19 +0200

    Migrate to script setup

--- app/src/components/PackageChart.vue
+++ app/src/components/PackageChart.vue
@@ -10,7 +10,8 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { defineProps, toRefs, ref, inject, onMounted, reactive, watch, onBeforeUnmount } from 'vue'
 import {
   Chart,
   LineElement,
@@ -21,172 +22,166 @@ import {
   Legend,
   Tooltip
 } from 'chart.js'
-
 import LoadingSpinner from './LoadingSpinner'
 
-export default {
-  inject: ['apiPackagesService', 'convertToDataSeries'],
-  props: {
-    packages: {
-      type: Array,
-      required: true
-    },
-    startMonth: {
-      type: Number,
-      required: false
-    },
-    endMonth: {
-      type: Number,
-      required: false
-    },
-    limit: {
-      type: Number,
-      required: false
-    }
+const apiPackagesService = inject('apiPackagesService')
+const convertToDataSeries = inject('convertToDataSeries')
+
+const props = defineProps({
+  packages: {
+    type: Array,
+    required: true
   },
-  data () {
-    return {
-      loading: true,
-      data: {
-        labels: [],
-        datasets: []
+  startMonth: {
+    type: Number,
+    required: false
+  },
+  endMonth: {
+    type: Number,
+    required: false
+  },
+  limit: {
+    type: Number,
+    required: false
+  }
+})
+const { packages, startMonth, endMonth, limit } = toRefs(props)
+
+const loading = ref(true)
+const errors = ref([])
+const data = reactive({
+  labels: [],
+  datasets: []
+})
+let chart = null
+
+const fetchData = () => {
+  if (packages.value.length < 1) {
+    loading.value = false
+    errors.value.push('No packages defined')
+    return
+  }
+  if (packages.value.length > 10) {
+    loading.value = false
+    errors.value.push('Too many packages defined')
+    return
+  }
+
+  loading.value = true
+  Promise.all(packages.value.map(pkg => apiPackagesService.fetchPackageSeries(pkg,
+    {
+      startMonth: startMonth.value,
+      endMonth: endMonth.value,
+      limit: limit.value
+    }).catch(error => {
+    errors.value.push(error)
+  })
+  ))
+    .then(dataArray => {
+      data.value = convertToDataSeries(dataArray)
+    })
+    .catch(error => {
+      errors.value.push(error)
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+
+const drawChart = () => {
+  const renderYearMonth = yearMonth => {
+    const yearMonthString = yearMonth.toString()
+    const year = yearMonthString.substr(0, 4)
+    const month = yearMonthString.substr(4, 2)
+    return `${year}-${month}`
+  }
+
+  const colors = ['#08c', '#dc3545', '#198754', '#ffc107', '#0dcaf0', '#d63384', '#fd7e14', '#333', '#6f42c1', '#adb5bd']
+  const textColor = '#333'
+
+  Chart.register(
+    LineElement,
+    PointElement,
+    LineController,
+    CategoryScale,
+    LinearScale,
+    Legend,
+    Tooltip
+  )
+
+  chart = new Chart('package-chart', {
+    type: 'line',
+    data: data.value,
+    options: {
+      interaction: {
+        mode: 'index',
+        intersect: false
       },
-      errors: [],
-      chart: null
-    }
-  },
-  components: {
-    LoadingSpinner
-  },
-  watch: {
-    packages: function () {
-      this.fetchData()
-    },
-    data: function () {
-      if (this.data.datasets.length > 0) {
-        this.drawChart()
-      }
-    }
-  },
-  methods: {
-    fetchData () {
-      if (this.packages.length < 1) {
-        this.loading = false
-        this.errors.push('No packages defined')
-        return
-      }
-      if (this.packages.length > 10) {
-        this.loading = false
-        this.errors.push('Too many packages defined')
-        return
-      }
-
-      this.loading = true
-      Promise.all(this.packages.map(pkg => this.apiPackagesService.fetchPackageSeries(pkg,
-        {
-          startMonth: this.startMonth,
-          endMonth: this.endMonth,
-          limit: this.limit
-        }).catch(error => {
-        this.errors.push(error)
-      })
-      ))
-        .then(dataArray => {
-          this.data = this.convertToDataSeries(dataArray)
-        })
-        .catch(error => {
-          this.errors.push(error)
-        })
-        .finally(() => {
-          this.loading = false
-        })
-    },
-    drawChart () {
-      const renderYearMonth = yearMonth => {
-        const yearMonthString = yearMonth.toString()
-        const year = yearMonthString.substr(0, 4)
-        const month = yearMonthString.substr(4, 2)
-        return `${year}-${month}`
-      }
-
-      const colors = ['#08c', '#dc3545', '#198754', '#ffc107', '#0dcaf0', '#d63384', '#fd7e14', '#333', '#6f42c1', '#adb5bd']
-      const textColor = '#333'
-
-      Chart.register(
-        LineElement,
-        PointElement,
-        LineController,
-        CategoryScale,
-        LinearScale,
-        Legend,
-        Tooltip
-      )
-
-      const ctx = this.$el.querySelector('#package-chart')
-      this.chart = new Chart(ctx, {
-        type: 'line',
-        data: this.data,
-        options: {
-          interaction: {
-            mode: 'index',
-            intersect: false
-          },
-          plugins: {
-            tooltip: {
-              displayColors: false,
-              itemSort: (a, b) => b.raw - a.raw,
-              callbacks: {
-                title: (title) => renderYearMonth(title[0].label)
-              }
-            },
-            legend: {
-              labels: {
-                color: textColor
-              }
-            }
-          },
-          normalized: true,
-          scales: {
-            x: {
-              ticks: {
-                callback: function (val) {
-                  const yearMonth = this.getLabelForValue(val)
-                  return renderYearMonth(yearMonth)
-                },
-                color: textColor,
-                autoSkipPadding: 30
-              },
-              grid: {
-                display: false
-              }
-            },
-            y: {
-              type: 'linear',
-              min: 0,
-              grid: {
-                borderDash: [1, 2]
-              },
-              ticks: {
-                color: textColor
-              }
-            }
-          },
-          elements: {
-            line: {
-              borderColor: colors
-            },
-            point: {
-              radius: 0,
-              hoverRadius: 4,
-              hoverBackgroundColor: textColor
-            }
+      plugins: {
+        tooltip: {
+          displayColors: false,
+          itemSort: (a, b) => b.raw - a.raw,
+          callbacks: {
+            title: (title) => renderYearMonth(title[0].label)
+          }
+        },
+        legend: {
+          labels: {
+            color: textColor
           }
         }
-      })
+      },
+      normalized: true,
+      scales: {
+        x: {
+          ticks: {
+            callback: function (val) {
+              const yearMonth = this.getLabelForValue(val)
+              return renderYearMonth(yearMonth)
+            },
+            color: textColor,
+            autoSkipPadding: 30
+          },
+          grid: {
+            display: false
+          }
+        },
+        y: {
+          type: 'linear',
+          min: 0,
+          grid: {
+            borderDash: [1, 2]
+          },
+          ticks: {
+            color: textColor
+          }
+        }
+      },
+      elements: {
+        line: {
+          borderColor: colors
+        },
+        point: {
+          radius: 0,
+          hoverRadius: 4,
+          hoverBackgroundColor: textColor
+        }
+      }
     }
-  },
-  mounted () {
-    this.fetchData()
-  }
+  })
 }
+
+onMounted(() => {
+  fetchData()
+})
+
+onBeforeUnmount(() => {
+  chart.destroy()
+})
+
+watch(() => data.value, () => {
+  if (data.value.datasets.length > 0) {
+    drawChart()
+  }
+})
 </script>
