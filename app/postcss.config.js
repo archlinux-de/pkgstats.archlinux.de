3edb5d0b09678692b1779e282cd18892ef88ceff
Pierre Schmitz <pierre@archlinux.de>
2022-04-17 10:54:06 +0200

    Use purgecss to reduce CSS payload

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,5 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    autoprefixer,
+    purgecss({
+      content: ['**/*.js', '**/*.html', '**/*.vue'],
+      variables: true
+    })
   ]
 }
