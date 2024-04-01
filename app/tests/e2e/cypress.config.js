27782d5b53fd62b1d4c35623a07350c5406f1d89
Pierre Schmitz <pierre@archlinux.de>
2024-04-01 12:13:29 +0200

    Fix Cypress permissions

--- app/tests/e2e/cypress.config.js
+++ app/tests/e2e/cypress.config.js
@@ -1,17 +1,18 @@
 module.exports = {
   fixturesFolder: false,
   screenshotOnRunFailure: false,
+  trashAssetsBeforeRuns: false,
   video: false,
   numTestsKeptInMemory: 0,
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   e2e: {
     setupNodeEvents (on, config) {}
   },
   component: {
     setupNodeEvents (on, config) {},
     specPattern: '**/*.cy.{js,jsx,ts,tsx}'
-  },
-  retries: {
-    runMode: 5,
-    openMode: 0
   }
 }
