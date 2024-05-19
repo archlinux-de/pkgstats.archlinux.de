318206750c39934504452a8cb5f614a0e1b8a820
Miriam Mueller <mail@miriam-mueller.com>
2024-05-19 19:15:28 +0200

    test basic rendering of system architecture page

---
+++ app/tests/e2e/cypress/e2e/CompareSystemArchitectures.cy.js
@@ -0,0 +1,15 @@
+describe('Compare System Architectures', () => {
+  beforeEach(() => {
+    cy.intercept({ method: 'GET', pathname: /^\/api\/system-architectures/ }).as('api-compare-system-architectures')
+    cy.visit('/compare/system-architectures')
+    cy.wait('@api-compare-system-architectures')
+  })
+
+  it('shows title', () => {
+    cy.contains('h1', 'Compare System Architectures')
+  })
+
+  it('shows chart', () => {
+    cy.assertCanvasIsNotEmpty('[data-test=system-architecture-chart][data-test-rendered=true][style]')
+  })
+})
