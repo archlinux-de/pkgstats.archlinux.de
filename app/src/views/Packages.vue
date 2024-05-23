ab22f98da39c7be6685efafd32ecdeee7899c3ee
Miriam Mueller <mail@miriam-mueller.com>
2024-05-23 20:57:03 +0200

    #107: wip add bootstrap-icons and use icons from package

--- app/src/views/Packages.vue
+++ app/src/views/Packages.vue
@@ -21,10 +21,7 @@
          data-test-name="comparison-graph-link"
       >
         Show graph
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-up-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5"/>
-          <path fill-rule="evenodd" d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0z"/>
-        </svg>
+        <span v-html="boxArrowUpRight" class="text-primary"></span>
       </a>
     </div>
 
@@ -62,15 +59,10 @@
           <td>
             <button data-test="toggle-pkg-in-comparison" class="btn" @click="togglePackageSelected(pkg.name)">
               <template v-if="selectedPackages.includes(pkg.name)">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                </svg>
+                <span v-html="trash" class="text-primary"></span>
               </template>
               <template v-else>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                </svg>
+                <span v-html="plus" class="text-primary"></span>
               </template>
             </button>
           </td>
@@ -98,6 +90,9 @@ import { useHead } from '@vueuse/head'
 import { useRouteHash, useRouteQuery } from '@vueuse/router'
 import LoadingSpinner from '../components/LoadingSpinner'
 import { useFetchPackageList } from '../composables/useFetchPackageList'
+import trash from 'bootstrap-icons/icons/trash.svg?raw'
+import plus from 'bootstrap-icons/icons/plus.svg?raw'
+import boxArrowUpRight from 'bootstrap-icons/icons/box-arrow-up-right.svg?raw'
 
 const query = useRouteQuery('query', useRouteHash('').value.replace(/^#query=/, ''))
 const offset = ref(0)
