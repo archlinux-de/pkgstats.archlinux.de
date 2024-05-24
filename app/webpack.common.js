dddb2e97a5c3aaad06c55100a910d6b7a9fbb065
Pierre Schmitz <pierre@archlinux.de>
2024-05-24 16:16:30 +0200

    Disable warnings for hydration mismatches in production

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -24,7 +24,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       __VUE_OPTIONS_API__: false,
-      __VUE_PROD_DEVTOOLS__: false
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
     }),
     new VueLoaderPlugin(),
     new CopyPlugin({
