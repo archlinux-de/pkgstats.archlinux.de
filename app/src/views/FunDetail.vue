cc5cbb0f645a996b1f11da44e7df0fd7b15e8a13
Miriam Mueller <mail@miriam-mueller.com>
2024-05-26 11:30:42 +0200

    #132: add package chart (popularity over time per package)

--- app/src/views/FunDetail.vue
+++ app/src/views/FunDetail.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container" role="main">
-    <h1 class="mb-4">{{ categoryParameter }} statistics</h1>
-      <packages-bar-chart :packages="pkgs"></packages-bar-chart>
+    <h1 class="mb-4">{{ category }} statistics</h1>
+    <packages-bar-chart :packages="pkgs"></packages-bar-chart>
+    <package-chart :limit="0" :packages="pkgs" :start-month="0"></package-chart>
   </div>
 </template>
 
@@ -10,6 +11,7 @@ import { useHead } from '@vueuse/head'
 import { useRoute } from 'vue-router'
 import PackagesBarChart from '../components/PackagesBarChart'
 import FunConfig from '../config/fun.json'
+import PackageChart from "../components/PackageChart.vue";
 
 const category = useRoute().params.category
 const pkgs = FunConfig[category]
