cd36993274f650c0ecde35e1d6be4f55d86a7de2
Miriam Mueller <miriam.mueller@trimexa.de>
2024-07-21 16:47:51 +0200

    #143: wip: fix reactivity

--- app/src/composables/useFetchPackagesPopularity.js
+++ app/src/composables/useFetchPackagesPopularity.js
@@ -1,30 +1,39 @@
-import { ref, unref } from 'vue'
+import { ref, unref, watch } from 'vue'
 import { useFetchPackagePopularity } from './useFetchPackagePopularity'
 
-const sortPackagesByPopularity = packagePopularities => packagePopularities.sort((a, b) => Math.sign(b.popularity - a.popularity))
+const sortPackagesByPopularity = (packagePopularities) =>
+  packagePopularities.sort((a, b) => Math.sign(b.popularity - a.popularity))
 
 /**
  * @param {ref<array>} pkgs
  * @returns {Promise<any>}
  */
 export const useFetchPackagesPopularity = (pkgs) => {
-  const data = ref(unref(pkgs).map(pkg => ({ name: pkg, popularity: 0.0 })))
+  const data = ref(unref(pkgs).map((pkg) => ({ name: pkg, popularity: 0.0 })))
   const isFinished = ref(false)
   const isFetching = ref(true)
   const error = ref([])
 
-  Promise.all(unref(pkgs).map(pkg => useFetchPackagePopularity(pkg)))
-    .then(results => {
-      data.value = sortPackagesByPopularity(results.map(result => unref(result.data)))
-      error.value = results.map(result => unref(result.error)).filter(error => error)
+  const fetchPkgData = () => Promise.all(
+    unref(pkgs).map((pkg) => useFetchPackagePopularity(pkg))
+  )
+    .then((results) => {
+      data.value = sortPackagesByPopularity(
+        results.map((result) => unref(result.data))
+      )
+      error.value = results
+        .map((result) => unref(result.error))
+        .filter((error) => error)
       isFinished.value = true
     })
-    .catch(e => {
+    .catch((e) => {
       error.value.push(e.toString())
     })
     .finally(() => {
       isFetching.value = false
     })
+
+  watch(() => pkgs, fetchPkgData, { immediate: true })
 
   return {
     data,
