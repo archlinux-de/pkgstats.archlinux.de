44eb2034e30ebb8ba80b27e8dc49f256bca46b1e
daniel <193309918+danielfrrrr@users.noreply.github.com>
2026-03-18 00:26:29 -0300

    fun: sort the categories alphabetically

--- internal/ui/fun/config.go
+++ internal/ui/fun/config.go
@@ -16,14 +16,6 @@ var Categories = []Category{
 		},
 	},
 	{
-		Name: "Editors",
-		Packages: []string{
-			"code", "e3", "ed", "emacs", "geany", "gedit", "helix", "kakoune", "kate",
-			"gnome-text-editor", "gobby", "leafpad", "micro", "mousepad", "nano", "neovim",
-			"notepadqq", "orbiton", "orbiton-nano", "scite", "vi", "vim", "vis", "xed", "zed",
-		},
-	},
-	{
 		Name: "Desktop Environments",
 		Packages: []string{
 			"budgie-desktop", "cinnamon", "cosmic-session", "cutefish-core", "deepin-session",
@@ -33,24 +25,17 @@ var Categories = []Category{
 		},
 	},
 	{
+		Name: "Editors",
+		Packages: []string{
+			"code", "e3", "ed", "emacs", "geany", "gedit", "helix", "kakoune", "kate",
+			"gnome-text-editor", "gobby", "leafpad", "micro", "mousepad", "nano", "neovim",
+			"notepadqq", "orbiton", "orbiton-nano", "scite", "vi", "vim", "vis", "xed", "zed",
+		},
+	},
+	{
 		Name: "File Managers",
 		Packages: []string{
 			"caja", "dolphin", "konqueror", "mc", "nautilus", "nemo", "pcmanfm", "thunar",
-		},
-	},
-	{
-		Name: "Linux Kernels",
-		Packages: []string{
-			"linux", "linux-hardened", "linux-lts", "linux-rt", "linux-rt-lts", "linux-zen",
-		},
-	},
-	{
-		Name: "Window Managers",
-		Packages: []string{
-			"awesome", "blackbox", "bspwm", "cage", "ctwm", "cwm", "dwm", "fluxbox", "fvwm3",
-			"herbstluftwm", "hyprland", "i3-wm", "icewm", "jwm", "labwc", "mangowc", "niri",
-			"notion", "openbox", "pekwm", "qtile", "ratpoison", "river", "spectrwm", "stumpwm",
-			"sway", "wayfire", "weston", "windowmaker", "xmonad", "xorg-twm",
 		},
 	},
 	{
@@ -58,40 +43,6 @@ var Categories = []Category{
 		Packages: []string{
 			"mesa", "nvidia-utils", "vulkan-broadcom", "vulkan-dzn", "vulkan-gfxstream",
 			"vulkan-intel", "vulkan-nouveau", "vulkan-radeon", "vulkan-swrast",
-		},
-	},
-	{
-		Name: "Shells",
-		Packages: []string{
-			"bash", "dash", "dunesh", "elvish", "fish", "nushell", "oil", "tcsh", "xonsh", "zsh",
-		},
-	},
-	{
-		Name: "Terminal Emulators",
-		Packages: []string{
-			"alacritty", "contour", "foot", "ghostty", "gnome-console", "gnome-terminal", "kitty",
-			"konsole", "lxterminal", "mate-terminal", "ptyxis", "rio", "rxvt-unicode", "terminator",
-			"wezterm", "xfce4-terminal", "xterm", "yakuake",
-		},
-	},
-	{
-		Name: "Xorg GPU Drivers",
-		Packages: []string{
-			"nvidia-utils", "xf86-video-amdgpu", "xf86-video-ati", "xf86-video-intel",
-			"xf86-video-nouveau", "xf86-video-openchrome", "xf86-video-vmware",
-		},
-	},
-	{
-		Name: "Runtime Environments",
-		Packages: []string{
-			"bash", "deno", "erlang-nox", "java-environment-common", "lua", "nodejs",
-			"perl", "php", "python", "ruby",
-		},
-	},
-	{
-		Name: "System Languages",
-		Packages: []string{
-			"clang", "gcc", "ghc", "go", "nim", "ocaml", "rust", "rustup", "vala", "zig",
 		},
 	},
 	{
@@ -105,9 +56,58 @@ var Categories = []Category{
 		},
 	},
 	{
+		Name: "Linux Kernels",
+		Packages: []string{
+			"linux", "linux-hardened", "linux-lts", "linux-rt", "linux-rt-lts", "linux-zen",
+		},
+	},
+	{
+		Name: "Runtime Environments",
+		Packages: []string{
+			"bash", "deno", "erlang-nox", "java-environment-common", "lua", "nodejs",
+			"perl", "php", "python", "ruby",
+		},
+	},
+	{
+		Name: "Shells",
+		Packages: []string{
+			"bash", "dash", "dunesh", "elvish", "fish", "nushell", "oil", "tcsh", "xonsh", "zsh",
+		},
+	},
+	{
+		Name: "System Languages",
+		Packages: []string{
+			"clang", "gcc", "ghc", "go", "nim", "ocaml", "rust", "rustup", "vala", "zig",
+		},
+	},
+	{
+		Name: "Terminal Emulators",
+		Packages: []string{
+			"alacritty", "contour", "foot", "ghostty", "gnome-console", "gnome-terminal", "kitty",
+			"konsole", "lxterminal", "mate-terminal", "ptyxis", "rio", "rxvt-unicode", "terminator",
+			"wezterm", "xfce4-terminal", "xterm", "yakuake",
+		},
+	},
+	{
 		Name: "Widget Toolkits",
 		Packages: []string{
 			"gtk3", "gtk4", "qt5-base", "qt6-base",
+		},
+	},
+	{
+		Name: "Window Managers",
+		Packages: []string{
+			"awesome", "blackbox", "bspwm", "cage", "ctwm", "cwm", "dwm", "fluxbox", "fvwm3",
+			"herbstluftwm", "hyprland", "i3-wm", "icewm", "jwm", "labwc", "mangowc", "niri",
+			"notion", "openbox", "pekwm", "qtile", "ratpoison", "river", "spectrwm", "stumpwm",
+			"sway", "wayfire", "weston", "windowmaker", "xmonad", "xorg-twm",
+		},
+	},
+	{
+		Name: "Xorg GPU Drivers",
+		Packages: []string{
+			"nvidia-utils", "xf86-video-amdgpu", "xf86-video-ati", "xf86-video-intel",
+			"xf86-video-nouveau", "xf86-video-openchrome", "xf86-video-vmware",
 		},
 	},
 }
